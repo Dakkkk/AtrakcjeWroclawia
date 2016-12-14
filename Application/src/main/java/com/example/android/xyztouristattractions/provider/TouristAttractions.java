60770ba52a7a412dcdcd4557dad1d42e8548a5e5
DaveKun <dawidkunicki88@gmail.com>
2016-12-14 11:20:58 +0100

    Dodanie atrakcji z opisem i wsp. dla atrakcji Wroclawia

--- Application/src/main/java/com/example/android/xyztouristattractions/provider/TouristAttractions.java
+++ Application/src/main/java/com/example/android/xyztouristattractions/provider/TouristAttractions.java
@@ -33,9 +33,9 @@ import java.util.Map;
  */
 public class TouristAttractions {
 
-    public static final String CITY_SYDNEY = "Sydney";
+    public static final String CITY_WROCLAW = "Wrocław";
 
-    public static final String TEST_CITY = CITY_SYDNEY;
+    public static final String TEST_CITY = CITY_WROCLAW;
 
     private static final float TRIGGER_RADIUS = 2000; // 2KM
     private static final int TRIGGER_TRANSITION = Geofence.GEOFENCE_TRANSITION_ENTER |
@@ -43,7 +43,7 @@ public class TouristAttractions {
     private static final long EXPIRATION_DURATION = Geofence.NEVER_EXPIRE;
 
     public static final Map<String, LatLng> CITY_LOCATIONS = new HashMap<String, LatLng>() {{
-        put(CITY_SYDNEY, new LatLng(-33.873651, 151.2068896));
+        put(CITY_WROCLAW, new LatLng(51.1078852, 17.03853760000004));
     }};
 
     /**
@@ -52,55 +52,74 @@ public class TouristAttractions {
     public static final HashMap<String, List<Attraction>> ATTRACTIONS =
             new HashMap<String, List<Attraction>>() {{
 
-        put(CITY_SYDNEY, new ArrayList<Attraction>() {{
+        put(CITY_WROCLAW, new ArrayList<Attraction>() {{
             add(new Attraction(
-                    "Sydney Opera House",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae bibendum justo, vitae cursus velit. Suspendisse potenti.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae bibendum justo, vitae cursus velit. Suspendisse potenti. Suspendisse scelerisque risus justo, non tincidunt nibh blandit et. Vivamus elit lacus, luctus nec erat in, pharetra semper turpis. Quisque viverra nulla ligula, non pulvinar ante dictum sit amet. Vestibulum aliquet tortor mauris, vel suscipit nisl malesuada eget. Aliquam maximus dictum euismod. Maecenas leo quam, volutpat id diam eget, placerat fringilla ipsum. Nam pretium vehicula augue quis euismod.\n\nNam sed blandit magna. Vestibulum a fermentum arcu. Vestibulum et ligula at nisi luctus facilisis. Proin fermentum enim a nibh commodo finibus. Suspendisse justo elit, vulputate ut ipsum at, pellentesque auctor massa. Praesent vestibulum erat interdum imperdiet dapibus. In hac habitasse platea dictumst. Proin varius orci vitae tempor vulputate.\n\nEtiam sed mollis orci. Integer et ex sed tortor scelerisque blandit semper id libero. Nulla facilisi. Pellentesque tempor magna eget massa ultrices, et efficitur lectus finibus.",
-                    Uri.parse("https://lh5.googleusercontent.com/-7fb5ybQhUbo/VGLWjIL4RmI/AAAAAAAAACM/2jLe_msj_tk/w600-no/IMG_0049.JPG"),
-                    Uri.parse("https://lh3.googleusercontent.com/-EFEw6s7mT6I/VGLkCH4Xt4I/AAAAAAAAADY/ZlznhaQvb8E/w600-no/DSC_2775.JPG"),
-                    new LatLng(-33.858667, 151.214028),
-                    CITY_SYDNEY
+                    "Rynek wrocławski",
+                    "Po pierwsze każdy, kto przyjeżdża do Wrocławia, MUSI zaliczyć spacer po Starym Mieście — Rynek i jego okolice, spacer nad Odrą w stronę Ostrowa Tumskiego i sam Ostrów Tumski, a później przez most w stronę Muzeum Narodowego i przez park z powrotem w stronę Rynku. To takie minimum wrocławskiego turysty..",
+                    "Wrocław znalazł sposób na to, żeby dzieci podczas takiego spaceru się nie nudziły. W całym Wrocławiu znajduje się prawie 300 niewielkich pomników różnych krasnali. Znajdują się one naprawdę w bardzo wielu miejscach, najwięcej jest ich właśnie w okolicach Rynku, więc kiedy wy spacerujecie i podziwiacie Wrocław, wasze dzieci z wypiekami na policzkach odnajdują kolejne krasnale. Wypróbowałam na Lili, która normalnie po 300 metrach spaceru krzyczy, że bolą ją nogi i, że chce na ręce. We Wrocławiu przez ponad 1 kilometr marszu ani razu nie powiedziała nic o sobie, bo była zbyt zajęta szukaniem i odnajdowaniem krasnali. Zresztą dorośli też krasnale bardzo lubią. Mój ulubiony to ten stojący przy bankomacie krasnoludzkiego oddziału Banku Zachodniego WBK :) Jeśli chcecie poczytać o krasnalach więcej, to zapraszam na krasnale.pl.",
+                    Uri.parse("http://mamygadzety.pl/wp-content/uploads/2016/01/dollarphotoclub_61018707.jpg"),
+                    Uri.parse("http://mamygadzety.pl/wp-content/uploads/2016/01/dollarphotoclub_61018707.jpg"),
+                    new LatLng(51.1078852, 17.03853760000004),
+                    CITY_WROCLAW
             ));
 
             add(new Attraction(
-                    "Sydney Harbour Bridge",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut nulla neque. Morbi nec felis vel neque rhoncus malesuada.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut nulla neque. Morbi nec felis vel neque rhoncus malesuada. Mauris non nisi est. Nunc in ipsum euismod, suscipit dolor eget, efficitur nisi. Integer venenatis mauris mauris, quis luctus risus pellentesque a. Duis tempus est at ligula vehicula fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\n\nNam ut sodales nibh, euismod aliquet lectus. Curabitur ornare dictum nisi, at faucibus magna. Morbi tempus nibh sed sodales volutpat. Etiam sodales, turpis sit amet porttitor tristique, libero libero faucibus est, viverra dictum risus ipsum vel augue. Nulla dolor magna, iaculis ac ornare id, fermentum eget massa. Sed mattis, odio nec sodales vehicula, neque metus ullamcorper nulla, sit amet ullamcorper risus lectus a ipsum. Curabitur venenatis feugiat quam nec elementum. Curabitur a interdum urna. Curabitur tincidunt tortor eget neque condimentum blandit. Etiam imperdiet, enim nec blandit convallis, nunc augue.",
-                    Uri.parse("https://lh6.googleusercontent.com/-ORRJtfLQlaw/VGLmQPv3n8I/AAAAAAAAAD8/2TzSCCPzl9k/w600-no/DSC04114.JPG"),
-                    Uri.parse("https://lh4.googleusercontent.com/-ch9Kk-7pD68/VGLkCNh5niI/AAAAAAAAADc/ztxkRHWX-po/w600-no/DSC_2739.JPG"),
-                    new LatLng(-33.852222, 151.210556),
-                    CITY_SYDNEY
+                    "Muzeum Narodowe",
+                    "Muzeum Narodowe we Wrocławiu – jedno z głównych muzeów Wrocławia i Dolnego Śląska. Zbiory muzeum obejmują przede wszystkim malarstwo i rzeźbę, ze szczególnym uwzględnieniem sztuki całego Śląska.",
+                    "Zbiory[edytuj]\n" +
+                            "Ekspozycja w gmachu głównym przy pl. Powstańców Warszawy jest podzielona na następujące działy:\n" +
+                            "\n" +
+                            "Śląska rzeźba kamienna XII–XVI w.[edytuj]\n" +
+                            "Zawiera m.in. nagrobki książąt śląskich, figury ołtarzowe, rzeźby i reliefy.\n" +
+                            "\n" +
+                            "Sztuka śląska XIV–XIX w.[edytuj]\n" +
+                            "Najcenniejsza część kolekcji Muzeum, zawiera przede wszystkim wysokiej klasy obiekty gotyckiej sztuki sakralnej przeniesione tu ze śląskich kościołów (rzeźby, obrazy, ołtarze), np. w stylu Madonn na Lwach, Pięknych Madonn, realizmu mieszczańskiego, tworzone pod wpływem Wita Stwosza i inne, uzupełnione o zabytki rzemiosła artystycznego[4]. Od dominującej sztuki sakralnej renesansu i baroku (na szczególną uwagę zasługują tu obrazy Michaela Willmanna przez sztukę świecką (portrety) z tych epok, klasycyzm, biedermeier, do początku XX wieku; obrazom i rzeźbom towarzyszy rzemiosło artystyczne (szkło, porcelana, fajans ze śląskich wytwórni)[5].\n" +
+                            "\n" +
+                            "Sztuka polska XVII–XIX w.[edytuj]\n" +
+                            "Główny trzon kolekcji pochodzi ze zbiorów muzealnych Lwowa (Galerii Miejskiej we Lwowie, Muzeum Narodowego im. Króla Jana III, Muzeum Lubomirskich) i Kijowa, przekazanych Wrocławiowi w 1946 r. Dominuje malarstwo, w tym dzieła Marcello Bacciarellego, Bernarda Bellotto (Canaletto), Jacka Malczewskiego, Jana Matejki, Piotra Michałowskiego, )[6].\n" +
+                            "\n" +
+                            "Sztuka europejska XV–XX w.[edytuj]\n",
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/b2/Muzeum_Narodowe%2C_Wroc%C5%82aw.jpg"),
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/6/6d/Muzeum_Narodowe_noc%C4%85_fot_BMaliszewska.jpg"),
+                    new LatLng(51.1109061,17.0476092),
+                    CITY_WROCLAW
             ));
 
             add(new Attraction(
-                    "Darling Harbour",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui in ipsum suscipit aliquet pretium aliquet odio.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui in ipsum suscipit aliquet pretium aliquet odio. Nam posuere nunc sed risus molestie varius. Suspendisse posuere faucibus urna, id vestibulum ante iaculis et. Vivamus placerat suscipit sem, a tempor nunc vehicula ac. Ut libero velit, dapibus sit amet euismod vel, dignissim a nisl.\n\nDonec non dui non felis laoreet malesuada. Fusce ac metus ultrices, fermentum felis quis, varius velit. Donec ac felis semper, scelerisque diam sed, dignissim risus. Maecenas vel semper sapien. Fusce euismod justo posuere, efficitur risus tincidunt, congue tellus. In hac habitasse platea dictumst. Sed lobortis risus consequat vehicula facilisis.\n\nIn hendrerit, neque in gravida rutrum, purus enim aliquet lectus, sit amet vulputate tortor lacus at sem. Aenean lorem metus, finibus rhoncus eros at, ullamcorper fringilla velit. Nulla vitae porttitor metus, quis gravida lectus. In rhoncus, diam a elementum luctus, erat nisi tempus ex, in porta est.",
-                    Uri.parse("https://lh5.googleusercontent.com/-qX43g6s92LY/VGLaTT3N35I/AAAAAAAAAC8/BbueQmch0Rw/w600-no/68001.jpg"),
-                    Uri.parse("https://lh6.googleusercontent.com/-SQ6T1Ure6l8/VGLaTg2iGuI/AAAAAAAAACo/m6_RkTW2G1o/w600-no/IMG_20140201_082851.jpg"),
-                    new LatLng(-33.8723, 151.19896),
-                    CITY_SYDNEY
+                    "Hala Targowa",
+                    "Hala Targowa we Wrocławiu (właśc. Hala Targowa nr I, niem. Markthalle Nummer 1) – zbudowana według projektu Richarda Plüddemanna i Heinricha Küstera w latach 1906–1908[a]. Mieści się przy ul. Piaskowej",
+                    "Hala Targowa we Wrocławiu (właśc. Hala Targowa nr I, niem. Markthalle Nummer 1) – zbudowana według projektu Richarda Plüddemanna i Heinricha Küstera w latach 1906–1908[a]. Mieści się przy ul. Piaskowej (Sandstraße), u zbiegu z pl. Nankiera (Ritterplatz) i ul. św. Ducha (Heiligegeiststraße), w bezpośredniej bliskości wyspy Piasek, mostu Piaskowego i najstarszych dzielnic miasta. Hala stanowi cenny zabytek sztuki inżynierskiej.\n" +
+                            "\n" +
+                            "Halę Targową nr I wzniesiono dla uporządkowania handlu w centrum miasta, który odbywał się wcześniej m.in. na Nowym Targu. Prawie identyczna Hala Targowa nr II tych samych projektantów powstała w tym samym czasie przy ul. Kolejowej, lecz po poważnym uszkodzeniu w 1945 została wyburzona w 1973.",
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Wroclaw_Hala_Targowa_plNankera.jpg/800px-Wroclaw_Hala_Targowa_plNankera.jpg"),
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/e/e1/Richard_Pl%C3%BCddemann_Market_Hall_photo_interior_1_Wroc%C5%82aw_Poland_2006-04-25.jpg"),
+                    new LatLng(51.1126439,17.0397772),
+                    CITY_WROCLAW
             ));
 
             add(new Attraction(
-                    "Bondi Beach",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et nunc in leo laoreet placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et nunc in leo laoreet placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin vestibulum laoreet odio nec posuere. Quisque ante arcu, malesuada vitae velit a, auctor tincidunt ante. Mauris varius eros eros, eget scelerisque mi scelerisque ut. Donec vehicula vitae urna ac hendrerit. Phasellus egestas risus nec euismod auctor.\n\nInteger fermentum velit et dolor varius sagittis. Proin et viverra sapien. Nulla aliquet ante et hendrerit egestas. Duis vulputate libero in nisi gravida cursus. Praesent laoreet nec dolor non iaculis. Aliquam eleifend ultricies ipsum, eu pellentesque libero rutrum non. Mauris et purus erat. Nullam semper mi id tincidunt viverra. Ut porta sem congue lectus luctus ultricies. Suspendisse iaculis lacinia nibh, eu accumsan magna volutpat vel. Sed id interdum mi, vel sollicitudin elit. Fusce facilisis elementum gravida. Duis at volutpat odio. Integer porta convallis tincidunt. Donec aliquam, leo ut.",
-                    Uri.parse("https://lh4.googleusercontent.com/-wbNgVdUkBiE/VHe99hGVtNI/AAAAAAAAAFY/fAHfhchNLJw/w600-no/IMG_20141124_143747.jpg"),
-                    Uri.parse("https://lh6.googleusercontent.com/-sjY_xlEOic4/VHe9-I4DD9I/AAAAAAAAAFI/Mt0VnjU7SxQ/w600-no/IMG_20141124_144008.jpg"),
-                    new LatLng(-33.89102, 151.277726),
-                    CITY_SYDNEY
+                    "Hala Stulecia",
+                    "Hala Stulecia (niem. Jahrhunderthalle, ang. Centennial Hall), daw. Hala Ludowa[2] – hala widowiskowo-sportowa znajdująca się we Wrocławiu, w dzielnicy Śródmieście, na osiedlu Zalesie, w parku Szczytnickim. Wzniesiona w latach 1911–1913 według projektu Maxa Berga, w stylu ekspresjonistycznym.",
+                    "Hala Stulecia (niem. Jahrhunderthalle, ang. Centennial Hall), daw. Hala Ludowa[2] – hala widowiskowo-sportowa znajdująca się we Wrocławiu, w dzielnicy Śródmieście, na osiedlu Zalesie, w parku Szczytnickim. Wzniesiona w latach 1911–1913 według projektu Maxa Berga, w stylu ekspresjonistycznym.\n" +
+                            "\n" +
+                            "W 2006 roku hala została uznana za obiekt światowego dziedzictwa UNESCO. Wpisana do rejestru zabytków w 1962 oraz ponownie w 1977, wraz z zespołem architektonicznym obejmującym m.in. Pawilon Czterech Kopuł, Pergolę i Iglicę.\n" +
+                            "\n" +
+                            "Obecnie hala i jej okolice są bardzo licznie odwiedzane przez zwiedzających nie tylko ze względu na samą halę, ale także na bliskość Pergoli z Fontanną Multimedialną, Ogrodem Japońskim oraz zoo.",
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Wroc%C5%82aw_-_Jahrhunderthalle1.jpg/1280px-Wroc%C5%82aw_-_Jahrhunderthalle1.jpg"),
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/3/31/Wroc%C5%82aw_-_Jahrhunderthalle6.jpg"),
+                    new LatLng(51.1068577,17.0772959),
+                    CITY_WROCLAW
             ));
 
             add(new Attraction(
-                    "Taronga Zoo",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros velit, faucibus in mi in, accumsan eleifend magna. Fusce efficitur volutpat leo nec finibus. Vivamus luctus quis dolor ac interdum.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros velit, faucibus in mi in, accumsan eleifend magna. Fusce efficitur volutpat leo nec finibus. Vivamus luctus quis dolor ac interdum. Donec iaculis, orci quis semper vulputate, tortor nisi porttitor tortor, at pretium ante quam ut odio. Donec fringilla sapien et dolor pharetra ultrices. Aenean faucibus felis non vulputate iaculis.\n\nEtiam eget dapibus ligula. Nunc facilisis dignissim tortor et elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam condimentum pellentesque mollis. Aliquam finibus urna ipsum, sed accumsan ante blandit quis. Vestibulum vel lacinia ligula. Nunc justo ex, volutpat nec justo ut, efficitur gravida lectus. Mauris cursus dui libero, vel tristique purus laoreet non.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices ullamcorper est, at consequat massa. Nam egestas at urna at pellentesque. Quisque lacus quam, efficitur vel erat eget, placerat feugiat eros. Mauris.",
-                    Uri.parse("https://lh6.googleusercontent.com/-kypwDfnk674/VGLWpQPm4VI/AAAAAAAAAB0/SrfL0fE9DnE/w500-no/OI000020_2.jpg"),
-                    Uri.parse("https://lh3.googleusercontent.com/-6_Ioko2ysgU/VHva2PjmRCI/AAAAAAAAAGM/cHjJC7ney4Q/w500-no/PC190054.JPG"),
-                    new LatLng(-33.843333, 151.241111),
-                    CITY_SYDNEY
+                    "Zoo",
+                    "Ogród Zoologiczny we Wrocławiu (Zoo Wrocław) – ogród zoologiczny znajdujący się przy ul. Wróblewskiego 1–5 we Wrocławiu, otwarty 10 lipca 1865.",
+                    "Ogród Zoologiczny we Wrocławiu (Zoo Wrocław) – ogród zoologiczny znajdujący się przy ul. Wróblewskiego 1–5 we Wrocławiu, otwarty 10 lipca 1865. Jest najstarszym[1] na obecnych ziemiach polskich ogrodem zoologicznym w Polsce. Powierzchnia ogrodu to 33 hektary[2]. Oficjalną nazwą ogrodu jest Zoo Wrocław Sp. z o.o.[3]\n" +
+                            "\n" + "Pod koniec 2015 wrocławskie Zoo prezentowało ponad 10 500 zwierząt (nie wliczając bezkręgowców) z 1132 gatunków[4]. Jest piątym najchętniej odwiedzanym ogrodem zoologicznym w Europie[5].",
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/0/09/1935_Brama_g%C5%82%C3%B3wna_teren%C3%B3w_wystawowych.jpg"),
+                    Uri.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Wroc%C5%82aw%2C_Miejski_Ogr%C3%B3d_Zoologiczny_-_fotopolska.eu_%28259320%29.jpg/1024px-Wroc%C5%82aw%2C_Miejski_Ogr%C3%B3d_Zoologiczny_-_fotopolska.eu_%28259320%29.jpg"),
+                    new LatLng(51.1041429,17.0742114),
+                    CITY_WROCLAW
             ));
         }});
 
